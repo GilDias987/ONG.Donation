ab7d36742a565862da3bcdf4a63d8ac1b4334fc8
Miguel De Oliveira Goncalves <9904319+Crefisa@prestadorserv.com.br>
2026-07-26 16:00:11 -0300

    program

--- ONG.Donation.WebAPI/Program.cs
+++ ONG.Donation.WebAPI/Program.cs
@@ -92,11 +92,8 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseHttpsRedirection();
